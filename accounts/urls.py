cc25bb1ee0a48eb198a8b8d3f00fdfaee32bcfa4
omid <omedmm3@gmail.com>
2024-06-06 22:13:39 +0430

    created logout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
 from django.contrib.auth.views import LoginView
+from django.urls import path, reverse_lazy
+
+from .views import logout
+
+app_name = "accounts"
 
 urlpatterns = [
-    path("login/", LoginView.as_view(template_name="accounts/login.html")),
+    path(
+        "login/", LoginView.as_view(template_name="accounts/login.html"), name="login"
+    ),
+    path("logout/", logout, name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+
+@login_required
+def logout(request):
+    auth_logout(request=request)
+    return redirect(reverse_lazy("accounts:login"))
