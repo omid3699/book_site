3291667cbd52f6fc8a07ced8c6c154a12d15a112
omid <omedmm3@gmail.com>
2024-10-30 13:03:16 +0430

    updated scripts

--- core/settings.py
+++ core/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "s/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 STATIC_ROOT = BASE_DIR / "static_files"
